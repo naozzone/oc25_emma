c69583f57321c0171ee5e8fb79bf84ab26f033ad
Emmiche <po03kyi@eduvaud.ch>
2026-05-14 15:56:45 +0200

    GPU parkour

--- 4_jeu/Projet_final/projet-final/move.gd
+++ 4_jeu/Projet_final/projet-final/move.gd
@@ -1,17 +1,20 @@
 @tool
 extends CSGBox3D
 
+@export var amplitude := 1.0
+@export var frequence := 1.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var t := 0.0
+var start_z := 0.0
+var initialized := false
 
-@export var amplitude := 1
-@export var frequence := 1
-
-var t = 0
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+
+	# Initialisation UNE seule fois
+	if not initialized:
+		start_z = position.z
+		initialized = true
+
 	t += delta
-	position.x = amplitude + sin(t * frequence * 2 * PI)
+
+	position.z = start_z + sin(t * frequence * 2.0 * PI) * amplitude
--- 4_jeu/Projet_final/projet-final/rotate.gd
+++ 4_jeu/Projet_final/projet-final/rotate.gd
@@ -11,4 +11,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rotation.x += delta * speed
+	rotation.y += delta * speed
